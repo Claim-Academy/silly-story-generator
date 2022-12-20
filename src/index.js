6631b7f3c7c0b81233da97535fddab381713736b
Manav Misra <manavm@visionwebsoft.com>
2022-12-20 11:00:30 -0600

    Add submit button component inside a form

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import TextInput from "./components/TextInput";
 import RadioInput from "./components/RadioInput";
+import Button from "./components/Button";
 
 const root = document.getElementById('root');
 
 root.innerHTML = `
-<main class="container mx-auto flex flex-col gap-y-4 items-center mt-8">
-  ${TextInput()}
-  <div class="flex gap-x-4">
-    ${RadioInput({label: "US", name: "country", checked: true})}
-    ${RadioInput({label: "UK", name: "country", checked: false})}
-  </div>
-</main>
-  `
+  <main class="container mx-auto mt-8">
+    <form class="flex flex-col gap-y-4 items-center">
+      ${TextInput()}
+      <div class="flex gap-x-4">
+        ${RadioInput({label: "US", name: "country", checked: true})}
+        ${RadioInput({label: "UK", name: "country", checked: false})}
+      </div>
+
+      ${Button("Submit")}
+    </form
+  </main>
+`
