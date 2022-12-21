ec03b85e22349c610a1e896999fc1caef616e29c
Manav Misra <manavm@visionwebsoft.com>
2022-12-21 13:29:27 -0600

    Clean up Form indenting

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,13 @@ import TextInput from "./TextInput";
 export default function Form() {
   return `
   <form class="flex flex-col gap-y-4 items-center">
-  ${TextInput()}
-  <div class="flex gap-x-4">
-    ${RadioInput({label: "US", name: "country", checked: true})}
-    ${RadioInput({label: "UK", name: "country", checked: false})}
-  </div>
+    ${TextInput()}
+    <div class="flex gap-x-4">
+      ${RadioInput({label: "US", name: "country", checked: true})}
+      ${RadioInput({label: "UK", name: "country", checked: false})}
+    </div>
 
-  ${Button("Submit")}
+    ${Button("Submit")}
 </form>
   `
 }
