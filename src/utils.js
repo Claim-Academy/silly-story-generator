b8dfb4562f695fa0e371b9df272c3b5907366f54
Manav Misra <manavm@visionwebsoft.com>
2022-12-21 13:54:05 -0600

    Add getRandomIntInclusive util fxn

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,9 @@ export const convertToKgs = (weight) => {
 export const convertToCelsius = (degrees) => {
   return (degrees - 32) * 5/9
 }
+
+export function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
